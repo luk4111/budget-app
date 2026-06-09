09d91e27818b61cf6aaf4e616c6dbd203202b6a4
luk4111 <zajaclukasz2003@gmail.com>
2026-06-09 20:36:32 +0200

    Initial monorepo setup: frontend + backend

--- pasir-l01-luk4111-Backend/src/main/java/pk/zl/pasir_zajac_lukasz/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package pk.zl.pasir_zajac_lukasz.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- pasir-l01-luk4111-Backend/src/main/java/pk/zl/pasir_zajac_lukasz/model/TransactionType.java
+++
@@ -1,6 +0,0 @@
-package pk.zl.pasir_zajac_lukasz.model;
-
-public enum TransactionType {
-    INCOME,
-    EXPENSE
-}
