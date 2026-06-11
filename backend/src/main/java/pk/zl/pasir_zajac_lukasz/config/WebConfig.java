e186fc20f88c18002937a9789eba4b3ae453b61f
luk4111 <zajaclukasz2003@gmail.com>
2026-06-11 21:03:56 +0200

    Initial monorepo setup: frontend + backend

--- backend/src/main/java/pk/zl/pasir_zajac_lukasz/config/WebConfig.java
+++ backend/src/main/java/pk/zl/pasir_zajac_lukasz/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins("http://localhost:5174","http://localhost:5175")
                 .allowedMethods("GET","PUT","POST","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
